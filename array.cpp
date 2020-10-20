e63b47de6e473c80dfd5b49c94cda92d87bcc422
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-20 22:55:21 +0500

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5];
+	for(int i=0;i<5;i++)
+	{
+		//cout<<"enter the array numbers"<<endl;
+		
+		cin>>arr[i];
+	}
+	for(int k=1;k<4;k++)
+{
+	cout<<k;
+}
+	return 0;
+}
